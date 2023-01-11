ef46dc127e44fce0c2745d294eb850cd01644092
natrorig <97001000+natrorig@users.noreply.github.com>
2023-01-11 17:32:21 -0300

    Update appTareas.js
    limpie un poco desafios 1, 2 y 3. Tratando de resolver 4

--- appTareas.js
+++ appTareas.js
@@ -1,62 +1,50 @@
 //TP para el 13 de Enero por NATALIA RIGAZIO//
-//DESAFIO 1//
+//  DESAFIO 1  //
 
-const tareas = [
-    {
-      Titulo: "Estudiar html",
-      Estado: "Terminado",
-    },
-    {
-      Titulo: "Estudiar CSS",
-      Estado: "En Progreso",
-    },
-    {
-      Titulo: "Estudiar JS",
-      Estado: "Pendiente",
-    },
+const tareas = [{Titulo: "Estudiar html", Estado: "Terminado",},
+    {Titulo: "Estudiar CSS",Estado: "En Progreso",},
+    {Titulo: "Estudiar JS",Estado: "Pendiente",},
   ];
   
-  console.log(tareas);
+console.log(tareas);
   
-  //DESAFIO 2 //
+//  DESAFIO 2  //
 
+function agregarTareas (titulo, estado) {
+    tareas.push ({Titulo: titulo, Estado: estado});
+    alert("Tarea agregada correctamente");
+    return tareas
+}
 
-  function agregarTareas (titulo, estado) {
-      tareas.push ({Titulo: titulo, Estado: estado});
-      alert("Tarea agregada correctamente");
-      return tareas
-    
-  }
-  console.log (agregarTareas("Repasar JS", "Pendiente"));
-  
-
+console.log (agregarTareas("Repasar JS", "Pendiente"));
 
 //   DESAFIO 3  //
   
-  function listarTareas (estado) {
+function listarTareas (estado) {
+    if (estado === "En Progreso" || estado === "Pendiente" || estado === "Terminado") {
+        const listadoTareas = tareas.filter(tareas => tareas.Estado === estado);
+        return listadoTareas;
   
-      if (estado === "En Progreso" || estado === "Pendiente" || estado === "Terminado") {
-          const listadoTareas = tareas.filter(tareas => tareas.Estado === estado);
-          return listadoTareas;
-  
-      } else {
-          return tareas;
-      }
-  }
-  
-  console.log(listarTareas("En Progreso"))
-  console.log(listarTareas("pepe"))
+    } else {
+        return tareas;
+    }
+}
+console.log(listarTareas("En Progreso"))
+console.log(listarTareas("pepe"))
   
 
-//   //DESAFIO 4 //
+//    DESAFIO 4 //
 
 function agregarTareas (titulo, estado) {
-    if (titulo === tareas.Titulo){
-        alert("Error: La tarea que intenta ingresar ya existe")
-    } else {
-    tareas.push ({Titulo: titulo, Estado: estado});
-    //alert("Tarea agregadna correctamente");
-    return tareas
+    for (let indice of tareas) { 
+        //iterar entre los indices de tareas para ver el titulo
+        if (titulo === tareas[indice].Titulo){
+            alert("Error: La tarea que intenta ingresar ya existe");  
+        } else {
+        tareas.push ({Titulo: titulo, Estado: estado});
+        alert("Tarea agregada correctamente");
+        return tareas
+        }
     }
 }
 console.log (agregarTareas("Repasar JS", "Pendiente"));
